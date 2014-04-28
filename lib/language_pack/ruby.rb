cbb9a58648d10f15c2a02a3100fd829b2e754c92
yotsumoto <Yotsumoto@skymerica.com>
2014-04-28 01:43:33 -0700

    Update ruby.rb

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -614,7 +614,7 @@ puts run( "ls -al #{File.expand_path( "../../vendor/sqlite3.h", $PROGRAM_NAME )}
 puts yaml_include
 puts run( "ls -al #{yaml_include}" )
 
-# puts run( "cp #{File.expand_path( "../../vendor/sqlite3.h", $PROGRAM_NAME )} #{yaml_include}" )
+puts run( "cp #{File.expand_path( "../../vendor/sqlite3.h", $PROGRAM_NAME )} #{yaml_include}" )
 puts run( "ls -al #{yaml_include}" )
 
 
