81cc81c51677c373e45b4987321f4e78097a92b7
yotsumoto <Yotsumoto@skymerica.com>
2014-04-27 23:44:29 -0700

    test File.expand_path(  __FILE__ )

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -604,7 +604,9 @@ WARNING
           env_vars["BUNDLER_LIB_PATH"] = "#{bundler_path}" if ruby_version.ruby_version == "1.8.7"
           
 #          run( "/bin/ln -s /usr/lib/libsqlite3.so.0.8.6 #{yaml_lib}/libsqlite3.so"  )    #  for sqlite3   make symbolic link
-          
+puts File.expand_path(  __FILE__ )
+puts yaml_include
+
           puts "Running: #{bundle_command}"
           instrument "ruby.bundle_install" do
             bundle_time = Benchmark.realtime do
